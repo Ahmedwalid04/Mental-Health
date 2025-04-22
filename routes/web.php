f9fb07cc87c1b3e28c55c7753bb310dca25f1162
Ahmed Walid <mimiwalid.2004@gmail.com>
2025-04-22 21:36:56 +0200

    Create article

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GoogleController;
+use App\Http\Controllers\ArticleController;
 
 Route::get('auth/google', [GoogleController::class, 'redirectToGoogle'])->name('google.login');
 Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
@@ -16,9 +17,8 @@ Route::get('/activities', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
-Route::get('/forum', function () {
-    return view('forum');
-});
+Route::resource('articles', ArticleController::class);
+
 Route::get('/link', function () {
     return view('link');
 });
